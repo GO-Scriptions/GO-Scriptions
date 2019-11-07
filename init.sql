94ea776690ac4851ad148ee3e05fd56b85c1dffa
fjhussai <farwajhussain@gmail.com>
2019-11-07 09:54:24 -0600

    created doctors table

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Doctors(
+    First_Name text,
+    Last_Name text,
+    Phone text,
+    Email text,
+    Username text NOT NULL,
+    Password text NOT NULL,
+    PRIMARY KEY (Username)
+);
