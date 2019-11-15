60f40824444f2f3af12a93cb3867c746002f0cc7
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-15 10:20:57 -0600

    minor changes for sanity's sake.

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE doctors (
+CREATE TABLE Doctors (
     docUsername varchar primary key,
     firstName varchar,
     lastName varchar,
@@ -37,8 +37,21 @@ CREATE TABLE Prescription_History (
     Patient_Last varchar,
     Cost decimal,
     Date_Prescribed date);
-INSERT INTO doctors (docUsername, firstName, lastName, docPassword) values ('drFarwa', 'Young', 'Farwa', 'thefarwacist');
-INSERT INTO doctors values ('PPotts','Pepper', 'Potts', 'mrsironman');
-INSERT INTO Pharmacists values ('Bruce', 'Banner', 'MrGreen', 'hulksmash','true');
-INSERT INTO Inventory values ('Ibuprofen', '5500', '1.25', 'Meditech');
-INSERT INTO Prescriptions values ('459056', 'Young Farwa', 'Amoxicillin', '500', 'Tony', 'Stark', '3.25', '2019 -10-31');
+
+INSERT INTO Doctors (docUsername, firstName, lastName, docPassword) 
+values ('drFarwa', 'Young', 'Farwa', 'thefarwacist');
+
+INSERT INTO doctors 
+values ('PPotts','Pepper', 'Potts', 'mrsironman');
+
+INSERT INTO Pharmacists 
+values ('Bruce', 'Banner', 'MrGreen', 'hulksmash','true');
+
+INSERT INTO Inventory 
+values ('Ibuprofen', '5500', '1.25', 'Meditech');
+
+INSERT INTO Prescriptions 
+values ('459056', 'Young Farwa', 'Amoxicillin', '500', 'Tony', 'Stark', '31.25', 'filled', '2018-10-31');
+
+INSERT INTO Prescription_History 
+values ('459056', 'Young Farwa', 'Amoxicillin', '500', 'Tony', 'Stark', '31.25', '2018-10-31');
